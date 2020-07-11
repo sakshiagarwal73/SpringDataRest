916a365f6c2f1d36b5ba9e42a3bd424eb8b68c4a
sakshi_1331 <sakshi.1331agarwal@gmail.com>
2020-07-11 23:29:06 +0530

    pushing repo

---
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,76 @@
+package com.luv2code.springboot.cruddemo.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+	
+	private EmployeeService employeeService;
+    
+	@Autowired
+	public EmployeeRestController(EmployeeService employeeService)
+	{
+		this.employeeService = employeeService;
+	}
+	
+	@GetMapping("/employees")
+	public List<Employee> findALL()
+	{
+		return employeeService.findAll();
+	}
+	
+	//add mapping for GET /employees/{employeeId}
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployeeById(@PathVariable int employeeId)
+	{
+		Employee employee = employeeService.findById(employeeId);
+		if(employee==null)
+		{
+			throw new RuntimeException("employee id not found " );
+		}
+		
+		return employee;
+	}
+	
+	// the @RequestBody annotation binds the HTTPRequest body to the domain object. 
+	//Spring framework automatically deserializes incoming HTTPRequest to the Java object 
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee employee)
+	{
+		employee.setId(0);
+		employeeService.save(employee);
+		return employee;
+	}
+	
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee employee)
+	{
+		employeeService.save(employee);
+		
+		return employee;
+	}
+	
+	//set delete mapping - /employees/{employeeId}
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId)
+	{
+		employeeService.delete(employeeId);
+		
+		return "deleted employee with id: " + employeeId;
+	}
+}
---
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.luv2code.springboot.cruddemo.service;
+
+import java.util.List;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+	
+    public List<Employee> findAll();
+    
+	public Employee findById(int id);
+	
+	public void save(Employee employee);
+	
+	public void delete(int id);
+
+}
